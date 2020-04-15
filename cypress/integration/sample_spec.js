6bf47e89633ce4d6461ee474545588572130bfd0
Tianle Huang <tianleh@amazon.com>
2020-04-15 15:08:03 -0700

    switch to watch on test branch

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,16 +1,16 @@
-// describe('Login succeeds', () => {
-//   it('successfully logs in', () => {
-//     cy.visit('http://localhost:5601')
-//     // change URL to match your dev URL
-//
-//     cy.get('input[name=username]', {timeout: 30000}).type('admin', {timeout: 30000, force: true})
-//
-//     // {enter} causes the form to submit
-//     cy.get('input[name=password]').type(`admin{enter}`, {timeout: 30000, force: true})
-//
-//     cy.url({timeout: 30000}).should('contain', '/home')
-//   })
-// })
+describe('Login succeeds', () => {
+  it('successfully logs in', () => {
+    cy.visit('http://localhost:5601')
+    // change URL to match your dev URL
+
+    cy.get('input[name=username]', {timeout: 30000}).type('admin', {timeout: 30000, force: true})
+
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`admin{enter}`, {timeout: 30000, force: true})
+
+    cy.url({timeout: 30000}).should('contain', '/home')
+  })
+})
 
 describe('Login fails', () => {
   it('logs in fails', () => {
