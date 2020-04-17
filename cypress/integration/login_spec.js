0845514e46d8ddb69224c5e58192bf913538367f
Tianle Huang <tianleh@amazon.com>
2020-04-17 16:58:51 -0700

    optimization

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,27 +1,27 @@
 describe('Login succeeds', () => {
   it('successfully logs in', () => {
-    cy.visit('http://localhost:5601')
+    cy.visit(`${Cypress.env('kibana')}`)
     // change URL to match your dev URL
 
-    cy.get('input[name=username]', {timeout: 30000}).type('admin', {timeout: 30000, force: true})
+    cy.get('input[name=username]').type('admin', {force: true})
 
     // {enter} causes the form to submit
-    cy.get('input[name=password]').type(`admin{enter}`, {timeout: 30000, force: true})
+    cy.get('input[name=password]').type('admin{enter}', {force: true})
 
-    cy.url({timeout: 30000}).should('contain', '/home')
+    cy.url().should('contain', '/home')
   })
 })
 
 describe('Login fails', () => {
   it('logs in fails', () => {
-    cy.visit('http://localhost:5601')
+    cy.visit(`${Cypress.env('kibana')}`)
     // change URL to match your dev URL
 
-    cy.get('input[name=username]', {timeout: 30000}).type('admin2', {timeout: 30000, force: true})
+    cy.get('input[name=username]').type('admin2', {force: true})
 
     // {enter} causes the form to submit
-    cy.get('input[name=password]').type(`admin{enter}`, {timeout: 30000, force: true})
+    cy.get('input[name=password]').type('admin{enter}', {force: true})
 
-    cy.get('.error-message', {timeout: 30000}).should('exist')
+    cy.get('.error-message').should('exist')
   })
 })
