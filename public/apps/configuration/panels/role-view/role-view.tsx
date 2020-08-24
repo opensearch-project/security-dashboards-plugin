556264303f185ee76753623f67f8d0f07604a91b
yuxitang-amzn <63078162+yuxitang-amzn@users.noreply.github.com>
2020-08-24 13:59:07 -0700

    Improve delete modal and apply to all listing pages (#396)

--- public/apps/configuration/panels/role-view/role-view.tsx
+++ public/apps/configuration/panels/role-view/role-view.tsx
@@ -159,17 +159,15 @@ export function RoleView(props: RoleViewProps) {
 
       setMappedUsers(difference(mappedUsers, selection));
       setSelection([]);
-      closeDeleteConfirmModal();
     } catch (e) {
       console.log(e);
     }
   };
 
-  const [
-    closeDeleteConfirmModal,
-    showDeleteConfirmModal,
-    deleteConfirmModal,
-  ] = useDeleteConfirmState(handleRoleMappingDelete, selection.length, 'mappings');
+  const [showDeleteConfirmModal, deleteConfirmModal] = useDeleteConfirmState(
+    handleRoleMappingDelete,
+    'mapping(s)'
+  );
 
   const message = (
     <EuiEmptyPrompt
