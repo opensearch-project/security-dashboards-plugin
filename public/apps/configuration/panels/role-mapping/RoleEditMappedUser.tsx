8f043f2d39ecefafa49711da0f7631a8dc3f5035
Yuxiang Tang <yuxitang@amazon.com>
2020-08-27 11:49:05 -0700

    Fix lint

--- public/apps/configuration/panels/role-mapping/RoleEditMappedUser.tsx
+++ public/apps/configuration/panels/role-mapping/RoleEditMappedUser.tsx
@@ -107,12 +107,20 @@ export function RoleEditMappedUser(props: RoleEditMappedUserProps) {
       };
 
       await updateRoleMapping(props.coreStart.http, props.roleName, updateObject);
-      setCrossPageToast(buildUrl(ResourceType.roles, Action.view, props.roleName, SubAction.mapuser), {
-        id: 'updateRoleMappingSucceeded',
-        color: 'success',
-        title: props.roleName + ' saved.',
-      });
-      window.location.href = buildHashUrl(ResourceType.roles, Action.view, props.roleName, SubAction.mapuser);
+      setCrossPageToast(
+        buildUrl(ResourceType.roles, Action.view, props.roleName, SubAction.mapuser),
+        {
+          id: 'updateRoleMappingSucceeded',
+          color: 'success',
+          title: props.roleName + ' saved.',
+        }
+      );
+      window.location.href = buildHashUrl(
+        ResourceType.roles,
+        Action.view,
+        props.roleName,
+        SubAction.mapuser
+      );
     } catch (e) {
       if (e.message) {
         addToast(createErrorToast('saveRoleMappingFailed', 'save error', e.message));
