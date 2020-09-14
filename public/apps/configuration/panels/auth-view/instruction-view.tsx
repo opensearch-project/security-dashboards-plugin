d26476ac606b1320b0adf1ada757c957d9fa3417
Tianle Huang <60111637+tianleh@users.noreply.github.com>
2020-09-14 11:17:07 -0700

    add unit test for authc & authz (#456)

---
+++ public/apps/configuration/panels/auth-view/instruction-view.tsx
@@ -0,0 +1,54 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+import { EuiCode, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
+import React from 'react';
+import { ExternalLinkButton } from '../../utils/display-utils';
+import { DocLinks } from '../../constants';
+
+export function InstructionView() {
+  return (
+    <>
+      <EuiTitle size="l">
+        <h1>Authentication and authorization</h1>
+      </EuiTitle>
+
+      <EuiSpacer size="xxl" />
+
+      <EuiText textAlign="center">
+        <h2>You have not set up authentication and authorization</h2>
+      </EuiText>
+
+      <EuiText textAlign="center" size="xs" color="subdued" className="instruction-text">
+        In order to use Security plugin, you must decide on authentication <EuiCode>authc</EuiCode>{' '}
+        and authorization backends <EuiCode>authz</EuiCode>. Use{' '}
+        <EuiCode>plugins/opendistro_security/securityconfig/config.yml</EuiCode> to define how to
+        retrieve and verify the user credentials, and how to fetch additional roles from backend
+        system if needed.
+      </EuiText>
+
+      <EuiSpacer />
+
+      <div style={{ textAlign: 'center' }}>
+        <ExternalLinkButton
+          fill
+          size="s"
+          href={DocLinks.BackendConfigurationDoc}
+          text="Create config.yml"
+        />
+      </div>
+    </>
+  );
+}
---
+++ public/apps/configuration/panels/auth-view/test/authorization-panel.test.tsx
@@ -0,0 +1,60 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import { AuthorizationPanel } from '../authorization-panel';
+import { EuiInMemoryTable } from '@elastic/eui';
+
+describe('Authorization panel', () => {
+  it('valid data', () => {
+    const authz = {
+      ldap: {
+        http_enabled: true,
+        transport_enabled: true,
+        authorization_backend: {
+          type: 'ldap',
+          config: {},
+        },
+      },
+      kerberos: {
+        http_enabled: false,
+        transport_enabled: false,
+        authorization_backend: {
+          type: 'intern',
+          config: {
+            kerberos_name: 'name',
+            kerberos_key: 'key',
+            kerberos_value: 'value',
+            kerberos_extensible: true,
+            kerberos_time: '100s',
+          },
+        },
+      },
+    };
+
+    const component = shallow(<AuthorizationPanel authz={authz} loading={false} />);
+
+    expect(component.find(EuiInMemoryTable).prop('items').length).toBe(2);
+  });
+
+  it('empty data', () => {
+    const authz = {};
+
+    const component = shallow(<AuthorizationPanel authz={authz} loading={false} />);
+    const foundTable = component.find(EuiInMemoryTable);
+    expect(foundTable.prop('items').length).toBe(0);
+  });
+});
