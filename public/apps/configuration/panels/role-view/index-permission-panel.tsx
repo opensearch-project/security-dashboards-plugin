e6ee1c4c6aa0a5e0ab301072c84b9bea3ed77fda
Tianle Huang <tianleh@amazon.com>
2020-10-08 16:53:11 -0700

    add fault tolerance to DLS rendering

--- public/apps/configuration/panels/role-view/index-permission-panel.tsx
+++ public/apps/configuration/panels/role-view/index-permission-panel.tsx
@@ -132,7 +132,15 @@ function getColumns(
           return EMPTY_FIELD_VALUE;
         }
 
-        return renderExpression('Document-level security', JSON.parse(dls));
+        // TODO: unify the experience for both cases which may require refactoring of renderExpression.
+        try {
+          return renderExpression('Document-level security', JSON.parse(dls));
+        } catch (e) {
+          // support the use case for $expression in DLS.
+
+          console.warn('Failed to parse dls as json!');
+          return dls;
+        }
       },
     },
     {
