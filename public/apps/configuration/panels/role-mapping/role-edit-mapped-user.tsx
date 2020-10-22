eebd1fd2a4501f99f53798e3f7b3ffb313681595
Tianle Huang <60111637+tianleh@users.noreply.github.com>
2020-10-22 11:18:08 -0700

    generalize user descriptions related to role mapping for external users (#553)

--- public/apps/configuration/panels/role-mapping/role-edit-mapped-user.tsx
+++ public/apps/configuration/panels/role-mapping/role-edit-mapped-user.tsx
@@ -141,7 +141,7 @@ export function RoleEditMappedUser(props: RoleEditMappedUserProps) {
             <h1>Map user</h1>
           </EuiTitle>
           Map users to this role to inherit role permissions. Two types of users are supported:
-          internal user, and external identity. <ExternalLink href={DocLinks.MapUsersToRolesDoc} />
+          user, and external identity. <ExternalLink href={DocLinks.MapUsersToRolesDoc} />
         </EuiText>
       </EuiPageHeader>
       <EuiSpacer size="m" />
