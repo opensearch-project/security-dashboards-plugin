c2cc3e524b5b072edf2d1f3e8a1469313f7f42cb
Yan Zeng <zengyan@amazon.com>
2020-10-30 14:57:15 -0700

    Add page refresh beind a config

--- public/plugin.ts
+++ public/plugin.ts
@@ -137,24 +137,28 @@ export class OpendistroSecurityPlugin
 
     setupTopNavButton(core, config);
 
-    // refresh the page when getting 401 unauthorized, e.g. when session timedo out.
-    core.http.intercept({
-      responseError: (httpErrorResponse, controller) => {
-        if (
-          httpErrorResponse.response?.status === 401 &&
-          !(
-            window.location.pathname.toLowerCase().includes('/login') ||
-            window.location.pathname.toLowerCase().includes('error')
-          )
-        ) {
-          if (config.auth.logout_url) {
-            window.location.href = config.auth.logout_url;
-          } else {
-            window.location.reload();
+    if (config.ui.autologout) {
+      // logout the user when getting 401 unauthorized, e.g. when session timed out.
+      core.http.intercept({
+        responseError: (httpErrorResponse, controller) => {
+          if (
+            httpErrorResponse.response?.status === 401 &&
+            !(
+              window.location.pathname.toLowerCase().includes('/login') ||
+              window.location.pathname.toLowerCase().includes('error')
+            )
+          ) {
+            if (config.auth.logout_url) {
+              window.location.href = config.auth.logout_url;
+            } else {
+              // when session timed out, user credentials in cookie are wiped out
+              // refres the page will direct the user to go through login process
+              window.location.reload();
+            }
           }
-        }
-      },
-    });
+        },
+      });
+    }
 
     return {};
   }
