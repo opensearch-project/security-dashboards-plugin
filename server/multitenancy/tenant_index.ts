8c8b33e48196ccfaebee5b366ccd355a22b9d6ce
Yan Zeng <zengyan@amazon.com>
2020-11-03 18:06:23 -0800

    Fail Kibana process when index migration failed

--- server/multitenancy/tenant_index.ts
+++ server/multitenancy/tenant_index.ts
@@ -123,7 +123,13 @@ export async function migrateTenantIndices(
       await indexMigrator.migrate();
     } catch (error) {
       logger.error(error);
-      throw error;
+      // fail early, exist the kibana process
+      // NOTE: according to https://github.com/elastic/kibana/issues/41983 ,
+      //       PR https://github.com/elastic/kibana/pull/75819 , API to allow plugins
+      //       to set status will be available in 7.10, for now, we fail Kibana
+      //       process to indicate index migration error. Customer can fix their
+      //       tenant indices in ES then restart Kibana.
+      process.exit(1);
     }
   }
 }
