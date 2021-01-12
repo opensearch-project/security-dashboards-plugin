a1e41a1d84672c97432f614058dcf87f79c3a5cf
Abbas Hussain <abbas_10690@yahoo.com>
2021-01-11 16:53:51 -0800

    Add new unauthenticated route for reporting stats API (#673)

--- server/index.ts
+++ server/index.ts
@@ -69,7 +69,7 @@ export const configSchema = schema.object({
       },
     }),
     anonymous_auth_enabled: schema.boolean({ defaultValue: false }),
-    unauthenticated_routes: schema.arrayOf(schema.string(), { defaultValue: ['/api/status'] }),
+    unauthenticated_routes: schema.arrayOf(schema.string(), { defaultValue: ['/api/status', '/api/reporting/stats'] }),
     forbidden_usernames: schema.arrayOf(schema.string(), { defaultValue: [] }),
     logout_url: schema.string({ defaultValue: '' }),
   }),
