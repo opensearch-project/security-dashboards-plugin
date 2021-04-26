03bff3cceb95625277472ccbb7be62dc3f78b3d5
Hsiang-Yu Cheng <hsiang@amazon.com>
2021-04-26 08:38:35 -0700

    Update rename_zip.js

--- build_tools/rename_zip.js
+++ build_tools/rename_zip.js
@@ -1,10 +1,26 @@
-const shell=require('child_process').execSync
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
 
-packageJson = require('../package.json')
-osdJson = require('../opensearch_dashboards.json')
 
-old_name = `build/${osdJson.id}-${osdJson.opensearchDashboardsVersion}.zip`
-new_name = `build/${packageJson.name}-${packageJson.version}.zip`
+const shell=require('child_process').execSync;
 
-console.log("rename "+old_name+" to "+new_name)
-shell(`mv ${old_name} ${new_name}`)
+packageJson = require('../package.json');
+osdJson = require('../opensearch_dashboards.json');
+
+old_name = `build/${osdJson.id}-${osdJson.opensearchDashboardsVersion}.zip`;
+new_name = `build/${packageJson.name}-${packageJson.version}.zip`;
+
+console.log("rename "+old_name+" to "+new_name);
+shell(`mv ${old_name} ${new_name}`);
