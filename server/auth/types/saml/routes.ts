8cc110db0f059b5b0df46517e196ae92bfcd9e92
Debjani Banerjee <bdebjani@amazon.com>
2021-05-03 12:34:54 -0700

    Fixing JSON parsing in SAML for strings that contain '\' character
    (cherry picked from commit 08d94f7dade0018005462df99a4d83c7cda70f4e)

--- server/auth/types/saml/routes.ts
+++ server/auth/types/saml/routes.ts
@@ -130,8 +130,9 @@ export class SamlAuthRoutes {
           if (!payloadEncoded) {
             context.security_plugin.logger.error('JWT token payload not found');
           }
-          const tokenPayload = JSON.parse(Buffer.from(payloadEncoded, 'base64').toString());
-
+          const tokenPayload = JSON.parse(
+            Buffer.from(payloadEncoded, 'base64').toString().replace('\\', '\\\\')
+          );
           if (tokenPayload.exp) {
             expiryTime = parseInt(tokenPayload.exp, 10) * 1000;
           }
@@ -188,8 +189,9 @@ export class SamlAuthRoutes {
           if (!payloadEncoded) {
             context.security_plugin.logger.error('JWT token payload not found');
           }
-          const tokenPayload = JSON.parse(Buffer.from(payloadEncoded, 'base64').toString());
-
+          const tokenPayload = JSON.parse(
+            Buffer.from(payloadEncoded, 'base64').toString().replace('\\', '\\\\')
+          );
           if (tokenPayload.exp) {
             expiryTime = parseInt(tokenPayload.exp, 10) * 1000;
           }
