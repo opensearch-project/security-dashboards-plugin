29774fc29da7606615034b8b661ec3fbbcba2fb8
Gio Collina <gio.collina@eliatra.com>
2021-10-25 21:37:50 +0200

    Disable private tenant for read only users
    Signed-off-by: Gio Collina <gio.collina@eliatra.com>

--- server/multitenancy/tenant_resolver.ts
+++ server/multitenancy/tenant_resolver.ts
@@ -37,10 +37,12 @@ export const GLOBAL_TENANTS: string[] = ['global', GLOBAL_TENANT_SYMBOL];
 export function resolveTenant(
   request: OpenSearchDashboardsRequest,
   username: string,
+  roles: string[] | undefined,
   availabeTenants: any,
   config: SecurityPluginConfigType,
   cookie: SecuritySessionCookie
 ): string | undefined {
+  const DEFAULT_READONLY_ROLES = ['kibana_read_only'];
   let selectedTenant: string | undefined;
   const securityTenant_ = request?.url?.searchParams?.get('securityTenant_');
   const securitytenant = request?.url?.searchParams?.get('securitytenant');
@@ -57,10 +59,13 @@ export function resolveTenant(
   } else {
     selectedTenant = undefined;
   }
+  const isReadonly = roles?.some((role) =>
+    config.readonly_mode?.roles.includes(role) || DEFAULT_READONLY_ROLES.includes(role)
+  );
 
   const preferredTenants = config.multitenancy?.tenants.preferred;
   const globalTenantEnabled = config.multitenancy?.tenants.enable_global || false;
-  const privateTenantEnabled = config.multitenancy?.tenants.enable_private || false;
+  const privateTenantEnabled = config.multitenancy?.tenants.enable_private && !isReadonly;
 
   return resolve(
     username,
