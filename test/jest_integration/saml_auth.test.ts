9af841e0d19a0e2ecc8f4eed86dc7a094e9c428e
Deepak Devarakonda <devardee@amazon.com>
2022-08-30 14:02:53 +0530

    Add one more test
    Signed-off-by: Deepak Devarakonda <devardee@amazon.com>

--- test/jest_integration/saml_auth.test.ts
+++ test/jest_integration/saml_auth.test.ts
@@ -16,7 +16,7 @@
 import * as osdTestServer from '../../../../src/core/test_helpers/osd_server';
 import { Root } from '../../../../src/core/server/root';
 import { resolve } from 'path';
-import { describe, expect, it, beforeAll, afterAll } from '@jest/globals';
+import { describe, expect, it, beforeAll, afterAll, afterEach, test } from '@jest/globals';
 import {
   ADMIN_CREDENTIALS,
   OPENSEARCH_DASHBOARDS_SERVER_USER,
@@ -24,7 +24,7 @@ import {
 } from '../constant';
 import wreck from '@hapi/wreck';
 import { Builder, By, until, ThenableWebDriver } from 'selenium-webdriver';
-
+import { Options } from 'selenium-webdriver/firefox';
 
 describe('start OpenSearch Dashboards server', () => {
   let root: Root;
@@ -61,6 +61,9 @@ describe('start OpenSearch Dashboards server', () => {
             anonymous_auth_enabled: false,
             type: 'saml',
           },
+          multitenancy: {
+            enabled: true,
+          },
         },
       },
       {
@@ -75,36 +78,35 @@ describe('start OpenSearch Dashboards server', () => {
     await root.start();
 
     console.log('Starting the Selenium Web Driver');
-    driver = new Builder().forBrowser('firefox').build();
-
-    await wreck.patch(
-      'https://localhost:9200/_plugins/_security/api/rolesmapping/all_access',
-      {
-        payload: [{
-          "op": "add",
-          "path": "/users",
-          "value": ["saml.jackson@example.com"]
-        }],
-        rejectUnauthorized: false,
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: ADMIN_CREDENTIALS,
+    driver = new Builder()
+      .forBrowser('firefox')
+      .setFirefoxOptions(new Options().headless())
+      .build();
+    await wreck.patch('https://localhost:9200/_plugins/_security/api/rolesmapping/all_access', {
+      payload: [
+        {
+          op: 'add',
+          path: '/users',
+          value: ['saml.jackson@example.com'],
         },
-      }
-    );
-    console.log("Starting to Download Flights Sample Data");
-    await wreck.post(
-      'http://localhost:5601/api/sample_data/flights',
-      {
-        payload: {},
-        rejectUnauthorized: false,
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: ADMIN_CREDENTIALS,
-        },
-      }
-    );
-    console.log("Downloaded Sample Data");
+      ],
+      rejectUnauthorized: false,
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: ADMIN_CREDENTIALS,
+      },
+    });
+    console.log('Starting to Download Flights Sample Data');
+    await wreck.post('http://localhost:5601/api/sample_data/flights', {
+      payload: {},
+      rejectUnauthorized: false,
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: ADMIN_CREDENTIALS,
+        security_tenant: 'global',
+      },
+    });
+    console.log('Downloaded Sample Data');
     const getConfigResponse = await wreck.get(
       'https://localhost:9200/_plugins/_security/api/securityconfig',
       {
@@ -142,111 +144,95 @@ describe('start OpenSearch Dashboards server', () => {
     };
     try {
       config.dynamic!.authc!.saml_auth_domain = saml_config;
-      await wreck.put(
-        'https://localhost:9200/_plugins/_security/api/securityconfig/config',
-        {
-          payload: config,
-          rejectUnauthorized: false,
-          headers: {
-            'Content-Type': 'application/json',
-            authorization: ADMIN_CREDENTIALS,
-          },
-        }
-      );
+      config.dynamic!.authc!.basic_internal_auth_domain.http_authenticator.challenge = false;
+      config.dynamic!.http!.anonymous_auth_enabled = false;
+      await wreck.put('https://localhost:9200/_plugins/_security/api/securityconfig/config', {
+        payload: config,
+        rejectUnauthorized: false,
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: ADMIN_CREDENTIALS,
+        },
+      });
     } catch (error) {
       console.log('Got an error!!');
     }
     console.log('The Config Response is : ' + JSON.stringify(config));
   });
 
-  afterAll( async () => {
-    console.log("Remove the Sample Data");
-    await wreck.delete(
-      'http://localhost:5601/api/sample_data/flights',
-      {
+  afterAll(async () => {
+    console.log('Remove the Sample Data');
+    await wreck
+      .delete('http://localhost:5601/api/sample_data/flights', {
         rejectUnauthorized: false,
         headers: {
           'Content-Type': 'application/json',
           authorization: ADMIN_CREDENTIALS,
         },
-      }
-    ).then(value => {
-      Promise.resolve(value);
-    }).catch(value => {
-      Promise.resolve(value);
-    });
-    console.log("Remove the Role Mapping");
-    await wreck.patch(
-      'https://localhost:9200/_plugins/_security/api/rolesmapping/all_access',
-      {
-        payload: [{
-          "op": "remove",
-          "path": "/users",
-          "users": ["saml.jackson@example.com"]
-        }],
+      })
+      .then((value) => {
+        Promise.resolve(value);
+      })
+      .catch((value) => {
+        Promise.resolve(value);
+      });
+    console.log('Remove the Role Mapping');
+    await wreck
+      .patch('https://localhost:9200/_plugins/_security/api/rolesmapping/all_access', {
+        payload: [
+          {
+            op: 'remove',
+            path: '/users',
+            users: ['saml.jackson@example.com'],
+          },
+        ],
         rejectUnauthorized: false,
         headers: {
           'Content-Type': 'application/json',
           authorization: ADMIN_CREDENTIALS,
         },
-      }
-    ).then(value => {
-      Promise.resolve(value);
-    }).catch(value => {
-      Promise.resolve(value);
-    });
-    console.log("Remove the Security Config");
-     await wreck.patch(
-      'https://localhost:9200/_plugins/_security/api/securityconfig',
-      {
-        payload: [{
-          "op": "remove",
-          "path": "/config/dynamic/authc/saml_auth_domain",
-        }],
+      })
+      .then((value) => {
+        Promise.resolve(value);
+      })
+      .catch((value) => {
+        Promise.resolve(value);
+      });
+    console.log('Remove the Security Config');
+    await wreck
+      .patch('https://localhost:9200/_plugins/_security/api/securityconfig', {
+        payload: [
+          {
+            op: 'remove',
+            path: '/config/dynamic/authc/saml_auth_domain',
+          },
+        ],
         rejectUnauthorized: false,
         headers: {
           'Content-Type': 'application/json',
           authorization: ADMIN_CREDENTIALS,
         },
-      }
-    ).then(value => {
-       Promise.resolve(value);
-     }).catch(value => {
-       Promise.resolve(value);
-     });
-     //   .then(value => {
-     //
-     // }).catch(reason => {
-     //
-     // });
+      })
+      .then((value) => {
+        Promise.resolve(value);
+      })
+      .catch((value) => {
+        Promise.resolve(value);
+      });
     // shutdown OpenSearchDashboards server
     await root.shutdown();
     await driver.quit();
-
   });
-  // 1 Integ Test for first time log in.
-  // 1 Integ Test for Log into Dashboard with Hash
-  // 1 Integ Test for logging into dev console
-  // 1 Integ Test to test Cookie expiry
+
   it('Login to app/opensearch_dashboards_overview#/ when SAML is enabled', async () => {
     await driver.get('http://localhost:5601/app/opensearch_dashboards_overview#/');
     await driver.findElement(By.id('btn-sign-in')).click();
-    // await driver.wait(until.urlContains('/app/home'));
-    // await driver.wait(until.titleIs('Home - OpenSearch Dashboards'));
-    // let currUrl = await driver.getCurrentUrl();
-    // console.log(currUrl);
-
-    await driver
-      .wait(
-        until.elementsLocated(
-          By.xpath("//*[@id=\"osdOverviewPageHeader__title\"]")
-        ),
-        10000
-      );
+    await driver.wait(
+      until.elementsLocated(By.xpath('//*[@id="osdOverviewPageHeader__title"]')),
+      10000
+    );
 
     let cookie = await driver.manage().getCookies();
-    console.log(cookie.length);
-    console.log(cookie[1]);
     expect(cookie.length).toEqual(2);
     await driver.manage().deleteAllCookies();
   });
@@ -255,24 +241,44 @@ describe('start OpenSearch Dashboards server', () => {
     await driver.get('http://localhost:5601/app/dev_tools#/console');
     await driver.findElement(By.id('btn-sign-in')).click();
 
-    await driver
-      .wait(
-        until.elementsLocated(
-          By.xpath("/html/body/div[1]/div/div/div/div[2]/div/main/div[1]/span/button/span")
-        ),
-        10000
-      );
+    await driver.wait(
+      until.elementsLocated(
+        By.xpath('/html/body/div[1]/div/div/div/div[2]/div/main/div[1]/span/button/span')
+      ),
+      10000
+    );
 
     let cookie = await driver.manage().getCookies();
     expect(cookie.length).toEqual(2);
     await driver.manage().deleteAllCookies();
   });
 
-});
+  it('Login to Dashboard with Hash', async () => {
+    let url_with_hash = `http://localhost:5601/app/dashboards#/view/7adfa750-4c81-11e8-b3d7-01146121b73d?_g=(filters:!(),refreshInterval:(pause:!f,value:900000),time:(from:now-24h,to:now))&_a=(description:'Analyze%20mock%20flight%20data%20for%20OpenSearch-Air,%20Logstash%20Airways,%20OpenSearch%20Dashboards%20Airlines%20and%20BeatsWest',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!t,title:'%5BFlights%5D%20Global%20Flight%20Dashboard',viewMode:view)`;
+    let driver2 = new Builder()
+      .forBrowser('firefox')
+      .setFirefoxOptions(new Options().headless())
+      .build();
+    await driver2.manage().deleteAllCookies();
+    await driver2.get(url_with_hash);
+    await driver2.wait(
+      until.elementsLocated(By.xpath('/html/body/nav/div/div[1]/a/i/span')),
+      60000
+    );
+    await driver2.findElement(By.xpath('//*[@id="btn-sign-in"]')).click();
+    await driver2.wait(
+      until.elementsLocated(By.xpath('/html/body/div[1]/div/header/div/div[2]')),
+      20000
+    );
+    let window_hash = await driver2.getCurrentUrl();
+    expect(window_hash).toEqual(url_with_hash);
+    let cookie = await driver2.manage().getCookies();
+    expect(cookie.length).toEqual(2);
+    await driver2.manage().deleteAllCookies();
+  });
 
-function sleepFor(sleepDuration: number) {
-  let now = new Date().getTime();
-  while (new Date().getTime() < now + sleepDuration) {
-    /* Do nothing */
-  }
-}
+  afterEach(async () => {
+    await driver.manage().deleteAllCookies();
+    console.log('Remove cookies from previous tests.');
+  });
+});
