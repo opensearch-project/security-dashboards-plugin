4e67d39a1732312ecefa8edfc9645d0d1f2b08c7
Chang Liu <lc12251109@gmail.com>
2022-10-26 16:56:40 -0700

    Resolve review comments (#1159)
    Signed-off-by: Chang Liu <lc12251109@gmail.com>

--- public/apps/account/tenant-switch-panel.tsx
+++ public/apps/account/tenant-switch-panel.tsx
@@ -92,10 +92,7 @@ export function TenantSwitchPanel(props: TenantSwitchPanelProps) {
         setUsername(currentUserName);
 
         let currentRawTenantName: string | undefined;
-        if (
-          props.config.multitenancy.enabled &&
-          props.config.multitenancy.enable_aggregation_view
-        ) {
+        if (props.config.multitenancy.enable_aggregation_view) {
           currentRawTenantName = props.tenant;
         } else {
           currentRawTenantName = accountInfo.data.user_requested_tenant;
