6d36c7f204ec326b6d69b3becf3fcc651593dcfc
nurbqq <nurbakhyt.sembayev@gmail.com>
2023-04-05 16:47:44 +0500

    Remove os check
    Signed-off-by: nurbqq <nurbakhyt.sembayev@gmail.com>

--- build_tools/rename_zip.js
+++ build_tools/rename_zip.js
@@ -23,13 +23,9 @@ const oldName = `build/${osdJson.id}-${osdJson.opensearchDashboardsVersion}.zip`
 const newName = `build/${pluginName}-${packageJson.version}.zip`;
 
 console.log('rename ' + oldName + ' to ' + newName);
-const os = require('os')
 
-if(os.type() == 'Windows_NT'){
-    const fs = require('fs');
-    fs.renameSync(oldName, newName);
-}else{
-    shell(`mv ${oldName} ${newName}`);
-}
+
+const fs = require('fs');
+fs.renameSync(oldName, newName);
 
 
