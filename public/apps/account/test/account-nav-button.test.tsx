965b982adeb08864abb3b22cf10d819f2346918a
Abhi Kalra <99718513+abhivka7@users.noreply.github.com>
2023-05-06 03:27:11 +0530

    Fixing dynamic tenancy changes for issues 1412 (#1419)
    * Fixing dynamic tenancy changes for opensearchdasbhoard.yaml
    
    Signed-off-by: Abhi Kalra <abhivka@amazon.com>
    Co-authored-by: Abhi Kalra <abhivka@amazon.com>
    Co-authored-by: Stephen Crawford <65832608+scrawfor99@users.noreply.github.com>
    Signed-off-by: leanneeliatra <leanne.laceybyrne@eliatra.com>

--- public/apps/account/test/account-nav-button.test.tsx
+++ public/apps/account/test/account-nav-button.test.tsx
@@ -171,7 +171,7 @@ describe('Account navigation button, multitenancy disabled', () => {
         currAuthType={'dummy'}
       />
     );
-    expect(setState).toBeCalledTimes(1);
+    expect(setState).toBeCalledTimes(0);
   });
 });
 
