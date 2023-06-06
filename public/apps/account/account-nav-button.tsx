d915bc7bd6f2ebdc2598f648b4a44d44123edb49
leanneeliatra <leanne.laceybyrne@eliatra.com>
2023-06-06 15:12:06 +0100

    Declared session storage as a constant
    Signed-off-by: leanneeliatra <leanne.laceybyrne@eliatra.com>

--- public/apps/account/account-nav-button.tsx
+++ public/apps/account/account-nav-button.tsx
@@ -191,8 +191,9 @@ export function reloadAfterTenantSwitch(): void {
   // the below portion is to clear URLs starting with 'lastUrl'
   // when switching tenants, the last URLs will be from the old tenancy therefore we need to remove these from sessionStorage.
   const lastUrls = [];
+  const { sessionStorage } = window;
 
-  for (let i = 0; i < window.sessionStorage.length; i++) {
+  for (let i = 0; i < sessionStorage.length; i++) {
     const key = sessionStorage.key(i);
     if (key?.startsWith('lastUrl')) {
       lastUrls.push(key);
