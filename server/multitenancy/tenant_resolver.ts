947d2d74816ef208a43e95343625d09f3913c122
Peter Nied <peternied@hotmail.com>
2023-06-30 08:30:18 -0500

    Fix a bad import path
    - Resolves #1487
    
    Signed-off-by: Peter Nied <petern@amazon.com>
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- server/multitenancy/tenant_resolver.ts
+++ server/multitenancy/tenant_resolver.ts
@@ -19,7 +19,7 @@ import { ResponseObject } from '@hapi/hapi';
 import { SecuritySessionCookie } from '../session/security_cookie';
 import { SecurityPluginConfigType } from '..';
 import { GLOBAL_TENANT_SYMBOL, PRIVATE_TENANT_SYMBOL, globalTenantName } from '../../common';
-import { modifyUrl } from '../../../../packages/osd-std';
+import { modifyUrl } from '@osd/std';
 import { ensureRawRequest } from '../../../../src/core/server/http/router';
 import { GOTO_PREFIX } from '../../../../src/plugins/share/common/short_url_routes';
 
