a9b26928938ba634debb9635c5bea56c63b33934
Stephen Crawford <steecraw@amazon.com>
2023-08-15 11:56:08 -0400

    Correct style fix timeouts
    Signed-off-by: Stephen Crawford <steecraw@amazon.com>

--- public/apps/configuration/panels/get-started.tsx
+++ public/apps/configuration/panels/get-started.tsx
@@ -14,24 +14,22 @@
  */
 
 import {
-    EuiButton,
-    EuiCode,
-    EuiFlexGroup,
-    EuiFlexItem,
-    EuiImage,
-    EuiPageHeader,
-    EuiPanel,
-    EuiSpacer,
-    EuiSteps,
-    EuiText,
-    EuiTitle,
+  EuiButton,
+  EuiCode,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPageHeader,
+  EuiPanel,
+  EuiSpacer,
+  EuiSteps,
+  EuiText,
+  EuiTitle,
 } from '@elastic/eui';
 import React from 'react';
 import { FormattedMessage } from '@osd/i18n/react';
 import { flow } from 'lodash';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { AppDependencies } from '../../types';
-import securityStepsDiagram from '../../../assets/get_started.svg';
 import { buildHashUrl, buildUrl } from '../utils/url-builder';
 import { Action, ResourceType, RouteItem } from '../types';
 import { API_ENDPOINT_CACHE, DocLinks } from '../constants';
@@ -42,244 +40,245 @@ import { getBreadcrumbs } from '../app-router';
 import { CrossPageToast } from '../cross-page-toast';
 
 const addBackendStep = {
-    title: 'Add backends',
-    children: (
-        <>
-            <EuiText size="s" color="subdued">
-                Add authentication<EuiCode>(authc)</EuiCode>and authorization<EuiCode>(authz)</EuiCode>
-                information to<EuiCode>config/opensearch-security/config.yml</EuiCode>. The
-                <EuiCode>authc</EuiCode> section contains the backends to check user credentials against.
-                The <EuiCode>authz</EuiCode>
-                section contains any backends to fetch backend roles from. The most common example of a
-                backend role is an LDAP group. <ExternalLink href={DocLinks.AuthenticationFlowDoc} />
-            </EuiText>
+  title: 'Add backends',
+  children: (
+    <>
+      <EuiText size="s" color="subdued">
+        Add authentication<EuiCode>(authc)</EuiCode>and authorization<EuiCode>(authz)</EuiCode>
+        information to<EuiCode>config/opensearch-security/config.yml</EuiCode>. The
+        <EuiCode>authc</EuiCode> section contains the backends to check user credentials against.
+        The <EuiCode>authz</EuiCode>
+        section contains any backends to fetch backend roles from. The most common example of a
+        backend role is an LDAP group. <ExternalLink href={DocLinks.AuthenticationFlowDoc} />
+      </EuiText>
 
-            <EuiSpacer size="m" />
+      <EuiSpacer size="m" />
 
-            <EuiFlexGroup gutterSize="s" wrap>
-                <EuiFlexItem grow={false}>
-                    <ExternalLinkButton
-                        fill
-                        href={DocLinks.BackendConfigurationDoc}
-                        text="Create config.yml"
-                    />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                    <EuiButton
-                        data-test-subj="review-authentication-and-authorization"
-                        onClick={() => {
-                            window.location.href = buildHashUrl(ResourceType.auth);
-                        }}
-                    >
-                        Review authentication and authorization
-                    </EuiButton>
-                </EuiFlexItem>
-            </EuiFlexGroup>
+      <EuiFlexGroup gutterSize="s" wrap>
+        <EuiFlexItem grow={false}>
+          <ExternalLinkButton
+            fill
+            href={DocLinks.BackendConfigurationDoc}
+            text="Create config.yml"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButton
+            data-test-subj="review-authentication-and-authorization"
+            onClick={() => {
+              window.location.href = buildHashUrl(ResourceType.auth);
+            }}
+          >
+            Review authentication and authorization
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
 
-            <EuiSpacer size="l" />
-        </>
-    ),
+      <EuiSpacer size="l" />
+    </>
+  ),
 };
 
 const setOfSteps = [
-    {
-        title: 'Create roles',
-        children: (
-            <>
-                <EuiText size="s" color="subdued">
-                    Roles are reusable collections of permissions. The default roles are a great starting
-                    point, but you might need to create custom roles that meet your exact needs.{' '}
-                    <ExternalLink href={DocLinks.CreateRolesDoc} />
-                </EuiText>
+  {
+    title: 'Create roles',
+    children: (
+      <>
+        <EuiText size="s" color="subdued">
+          Roles are reusable collections of permissions. The default roles are a great starting
+          point, but you might need to create custom roles that meet your exact needs.{' '}
+          <ExternalLink href={DocLinks.CreateRolesDoc} />
+        </EuiText>
 
-                <EuiSpacer size="m" />
+        <EuiSpacer size="m" />
 
-                <EuiFlexGroup gutterSize="s">
-                    <EuiFlexItem grow={false}>
-                        <EuiButton
-                            data-test-subj="explore-existing-roles"
-                            fill
-                            onClick={() => {
-                                window.location.href = buildHashUrl(ResourceType.roles);
-                            }}
-                        >
-                            Explore existing roles
-                        </EuiButton>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                        <EuiButton
-                            data-test-subj="create-new-role"
-                            onClick={() => {
-                                window.location.href = buildHashUrl(ResourceType.roles, Action.create);
-                            }}
-                        >
-                            Create new role
-                        </EuiButton>
-                    </EuiFlexItem>
-                </EuiFlexGroup>
+        <EuiFlexGroup gutterSize="s">
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="explore-existing-roles"
+              fill
+              onClick={() => {
+                window.location.href = buildHashUrl(ResourceType.roles);
+              }}
+            >
+              Explore existing roles
+            </EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="create-new-role"
+              onClick={() => {
+                window.location.href = buildHashUrl(ResourceType.roles, Action.create);
+              }}
+            >
+              Create new role
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
 
-                <EuiSpacer size="l" />
-            </>
-        ),
-    },
-    {
-        title: 'Map users',
-        children: (
-            <>
-                <EuiText size="s" color="subdued">
-                    After a user successfully authenticates, the security plugin retrieves that user’s roles.
-                    You can map roles directly to users, but you can also map them to backend roles.{' '}
-                    <ExternalLink href={DocLinks.MapUsersToRolesDoc} />
-                </EuiText>
+        <EuiSpacer size="l" />
+      </>
+    ),
+  },
+  {
+    title: 'Map users',
+    children: (
+      <>
+        <EuiText size="s" color="subdued">
+          After a user successfully authenticates, the security plugin retrieves that user’s roles.
+          You can map roles directly to users, but you can also map them to backend roles.{' '}
+          <ExternalLink href={DocLinks.MapUsersToRolesDoc} />
+        </EuiText>
 
-                <EuiSpacer size="m" />
+        <EuiSpacer size="m" />
 
-                <EuiFlexGroup gutterSize="s">
-                    <EuiFlexItem grow={false}>
-                        <EuiButton
-                            data-test-subj="map-users-to-role"
-                            fill
-                            onClick={() => {
-                                window.location.href = buildHashUrl(ResourceType.users);
-                            }}
-                        >
-                            Map users to a role
-                        </EuiButton>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                        <EuiButton
-                            data-test-subj="create-internal-user"
-                            onClick={() => {
-                                window.location.href = buildHashUrl(ResourceType.users, Action.create);
-                            }}
-                        >
-                            Create internal user
-                        </EuiButton>
-                    </EuiFlexItem>
-                </EuiFlexGroup>
-            </>
-        ),
-    },
+        <EuiFlexGroup gutterSize="s">
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="map-users-to-role"
+              fill
+              onClick={() => {
+                window.location.href = buildHashUrl(ResourceType.users);
+              }}
+            >
+              Map users to a role
+            </EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="create-internal-user"
+              onClick={() => {
+                window.location.href = buildHashUrl(ResourceType.users, Action.create);
+              }}
+            >
+              Create internal user
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </>
+    ),
+  },
 ];
 
 export function GetStarted(props: AppDependencies) {
-    let steps;
-    if (props.config.ui.backend_configurable) {
-        steps = [addBackendStep, ...setOfSteps];
-    } else {
-        steps = setOfSteps;
-    }
+  let steps;
+  if (props.config.ui.backend_configurable) {
+    steps = [addBackendStep, ...setOfSteps];
+  } else {
+    steps = setOfSteps;
+  }
 
-    return (
-        <>
-            <div className="panel-restrict-width">
-                <EuiPageHeader>
-                    <EuiTitle size="l">
-                        <h1>Get started</h1>
-                    </EuiTitle>
-                    <ExternalLinkButton text="Open in new window" href={buildHashUrl()} />
-                </EuiPageHeader>
+  return (
+    <>
+      <div className="panel-restrict-width">
+        <EuiPageHeader>
+          <EuiTitle size="l">
+            <h1>Get started</h1>
+          </EuiTitle>
+          <ExternalLinkButton text="Open in new window" href={buildHashUrl()} />
+        </EuiPageHeader>
 
-                <EuiPanel paddingSize="l">
-                    <EuiText size="s" color="subdued">
-                        <p>
-                            The OpenSearch security plugin lets you define the API calls that users can make and
-                            the data they can access. The most basic configuration consists of these steps.
-                        </p>
-                    </EuiText>
+        <EuiPanel paddingSize="l">
+          <EuiText size="s" color="subdued">
+            <p>
+              The OpenSearch security plugin lets you define the API calls that users can make and
+              the data they can access. The most basic configuration consists of these steps.
+            </p>
+          </EuiText>
 
-                    <EuiSpacer size="l" />
-                    {props.config.ui.backend_configurable && <div className={'security-steps-diagram'} />}
-                    <EuiSpacer size="l" />
+          <EuiSpacer size="l" />
+          {props.config.ui.backend_configurable && <div className={'security-steps-diagram'} />}
 
-                    <EuiSteps steps={steps} />
-                </EuiPanel>
+          <EuiSpacer size="l" />
 
-                <EuiSpacer size="l" />
+          <EuiSteps steps={steps} />
+        </EuiPanel>
 
-                <EuiPanel paddingSize="l">
-                    <EuiTitle size="s">
-                        <h3>Optional: Configure audit logs</h3>
-                    </EuiTitle>
-                    <EuiText size="s" color="subdued">
-                        <p>
-                            <FormattedMessage
-                                id="audit.logs.introduction"
-                                defaultMessage="Audit logs let you track user access to your OpenSearch cluster and are useful for compliance purposes."
-                            />{' '}
-                            <ExternalLink href={DocLinks.AuditLogsDoc} />
-                        </p>
-                        <EuiButton
-                            data-test-subj="review-audit-log-configuration"
-                            fill
-                            onClick={() => {
-                                window.location.href = buildHashUrl(ResourceType.auditLogging);
-                            }}
-                        >
-                            Review Audit Log Configuration
-                        </EuiButton>
-                    </EuiText>
-                </EuiPanel>
+        <EuiSpacer size="l" />
 
-                <EuiSpacer size="l" />
+        <EuiPanel paddingSize="l">
+          <EuiTitle size="s">
+            <h3>Optional: Configure audit logs</h3>
+          </EuiTitle>
+          <EuiText size="s" color="subdued">
+            <p>
+              <FormattedMessage
+                id="audit.logs.introduction"
+                defaultMessage="Audit logs let you track user access to your OpenSearch cluster and are useful for compliance purposes."
+              />{' '}
+              <ExternalLink href={DocLinks.AuditLogsDoc} />
+            </p>
+            <EuiButton
+              data-test-subj="review-audit-log-configuration"
+              fill
+              onClick={() => {
+                window.location.href = buildHashUrl(ResourceType.auditLogging);
+              }}
+            >
+              Review Audit Log Configuration
+            </EuiButton>
+          </EuiText>
+        </EuiPanel>
 
-                <EuiPanel paddingSize="l">
-                    <EuiTitle size="s">
-                        <h3>Optional: Purge cache</h3>
-                    </EuiTitle>
-                    <EuiText size="s" color="subdued">
-                        <p>
-                            By default, the security plugin caches authenticated users, along with their roles and
-                            permissions. This option will purge cached users, roles and permissions.
-                        </p>
-                        <EuiButton
-                            iconType="refresh"
-                            fill
-                            onClick={() => {
-                                props.coreStart.http.delete(API_ENDPOINT_CACHE);
-                            }}
-                        >
-                            Purge cache
-                        </EuiButton>
-                    </EuiText>
-                </EuiPanel>
+        <EuiSpacer size="l" />
 
-                <EuiSpacer size="l" />
+        <EuiPanel paddingSize="l">
+          <EuiTitle size="s">
+            <h3>Optional: Purge cache</h3>
+          </EuiTitle>
+          <EuiText size="s" color="subdued">
+            <p>
+              By default, the security plugin caches authenticated users, along with their roles and
+              permissions. This option will purge cached users, roles and permissions.
+            </p>
+            <EuiButton
+              iconType="refresh"
+              fill
+              onClick={() => {
+                props.coreStart.http.delete(API_ENDPOINT_CACHE);
+              }}
+            >
+              Purge cache
+            </EuiButton>
+          </EuiText>
+        </EuiPanel>
 
-                <EuiPanel paddingSize="l">
-                    <EuiTitle size="s">
-                        <h3>Optional: Multi-tenancy</h3>
-                    </EuiTitle>
-                    <EuiText size="s" color="subdued">
-                        <p>
-                            By default tenancy is activated in Dashboards. Tenants in OpenSearch Dashboards are
-                            spaces for saving index patterns, visualizations, dashboards, and other OpenSearch
-                            Dashboards objects.
-                        </p>
-                        <EuiFlexGroup gutterSize="s">
-                            <EuiFlexItem grow={false}>
-                                <EuiButton
-                                    fill
-                                    onClick={() => {
-                                        window.location.href = buildHashUrl(ResourceType.tenants);
-                                    }}
-                                >
-                                    Manage Multi-tenancy
-                                </EuiButton>
-                            </EuiFlexItem>
-                            <EuiFlexItem grow={false}>
-                                <EuiButton
-                                    onClick={() => {
-                                        window.location.href = buildHashUrl(ResourceType.tenantsConfigureTab);
-                                    }}
-                                >
-                                    Configure Multi-tenancy
-                                </EuiButton>
-                            </EuiFlexItem>
-                        </EuiFlexGroup>
-                    </EuiText>
-                </EuiPanel>
-            </div>
-        </>
-    );
+        <EuiSpacer size="l" />
+
+        <EuiPanel paddingSize="l">
+          <EuiTitle size="s">
+            <h3>Optional: Multi-tenancy</h3>
+          </EuiTitle>
+          <EuiText size="s" color="subdued">
+            <p>
+              By default tenancy is activated in Dashboards. Tenants in OpenSearch Dashboards are
+              spaces for saving index patterns, visualizations, dashboards, and other OpenSearch
+              Dashboards objects.
+            </p>
+            <EuiFlexGroup gutterSize="s">
+              <EuiFlexItem grow={false}>
+                <EuiButton
+                  fill
+                  onClick={() => {
+                    window.location.href = buildHashUrl(ResourceType.tenants);
+                  }}
+                >
+                  Manage Multi-tenancy
+                </EuiButton>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiButton
+                  onClick={() => {
+                    window.location.href = buildHashUrl(ResourceType.tenantsConfigureTab);
+                  }}
+                >
+                  Configure Multi-tenancy
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiText>
+        </EuiPanel>
+      </div>
+    </>
+  );
 }
