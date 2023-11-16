eb146dbad2fac5df6774b01b554c25fbb601e0be
leanne.laceybyrne@eliatra.com <leanne.laceybyrne@eliatra.com>
2023-11-16 10:39:34 +0000

    funtions extracted for unit testing
    Signed-off-by: leanne.laceybyrne@eliatra.com <leanne.laceybyrne@eliatra.com>

--- public/services/shared-link.ts
+++ public/services/shared-link.ts
@@ -12,15 +12,16 @@
  *   express or implied. See the License for the specific language governing
  *   permissions and limitations under the License.
  */
-
 import { parse } from 'url';
 import { CoreStart } from 'opensearch-dashboards/public';
 import { API_ENDPOINT_MULTITENANCY } from '../apps/configuration/constants';
 
 export async function addTenantToShareURL(core: CoreStart) {
   let tenant = '';
+
   try {
     tenant = await core.http.get(API_ENDPOINT_MULTITENANCY);
+
     if (!tenant) {
       tenant = 'global';
     } else if (tenant === '__user__') {
@@ -30,78 +31,79 @@ export async function addTenantToShareURL(core: CoreStart) {
     console.log(`failed to get user tenant: ${error}`);
     return;
   }
-  // Add the tenant to URLs copied from the share panel
+
   document.addEventListener('copy', (event) => {
-    const shareButton = document.querySelector('[data-share-url]') as any;
-    const target = document.querySelector('body > span');
-    shareButton.removeAllRanges();
-
-    // The copy event listens to Cmd + C too, so we need to make sure
-    // that we're actually copied something via the share panel
-    if (
-      shareButton &&
-      target &&
-      shareButton.getAttribute('data-share-url') === target.textContent
-    ) {
-      const originalValue = target.textContent;
-      let urlPart = originalValue;
-
-      // We need to figure out where in the value to add the tenant.
-      // Since OpenSearchDashboards sometimes adds values that aren't in the current location/url,
-      // we need to use the actual input values to do a sanity check.
-      try {
-        // For the iFrame urls we need to parse out the src
-        if (originalValue && originalValue.toLowerCase().indexOf('<iframe') === 0) {
-          const regex = /<iframe[^>]*src="([^"]*)"/i;
-          const match = regex.exec(originalValue);
-          if (match) {
-            urlPart = match[1]; // Contains the matched src, [0] contains the string where the match was found
-          }
-        }
-
-        const range = document.createRange() as any;
-        const referenceNode = document.getElementsByTagName('span').item(0);
-
-        range.selectNode(referenceNode);
-        shareButton.addRange(range);
-
-        const newValue = addTenantToURL(urlPart!, originalValue!, tenant);
-
-        if (newValue !== originalValue) {
-          target.textContent = newValue;
-        }
-      } catch (error) {
-        // Probably wasn't an url, so we just ignore this
-      }
-    }
+    processCopyEvent(tenant);
   });
 }
 
-function addTenantToURL(
-  url: string,
+export function processCopyEvent(userRequestedTenant: string) {
+  const shareButton = document.querySelector('[data-share-url]') as any;
+  const target = document.querySelector('body > span');
+
+  // The copy event listens to Cmd + C too, so we need to make sure
+  // that we're actually copied something via the share panel
+  if (shareButton && target && shareButton.getAttribute('data-share-url') === target.textContent) {
+    const originalValue = target.textContent;
+    let urlPart = originalValue;
+
+    // We need to figure out where in the value to add the tenant.
+    // Since OpenSearchDashboards sometimes adds values that aren't in the current location/url,
+    // we need to use the actual input values to do a sanity check.
+    try {
+      // For the iFrame urls we need to parse out the src
+      if (originalValue && originalValue.toLowerCase().indexOf('<iframe') === 0) {
+        const regex = /<iframe[^>]*src="([^"]*)"/i;
+        const match = regex.exec(originalValue);
+        if (match) {
+          urlPart = match[1]; // Contains the matched src, [0] contains the string where the match was found
+        }
+      }
+
+      updateClipboard(urlPart, originalValue, userRequestedTenant);
+    } catch (error) {
+      // Probably wasn't an url, so we just ignore this
+    }
+  }
+}
+
+export function updateClipboard(
+  urlPart: string,
   originalValue: string | undefined,
-  userRequestedTenant: string
+  tenant: string
 ) {
-  const tenantKey = 'security_tenant';
-  const tenantKeyAndValue = tenantKey + '=' + encodeURIComponent(userRequestedTenant);
+  const shareButton = document.querySelector('[data-share-url]') as any;
+  const target = document.querySelector('body > span');
 
   if (!originalValue) {
-    originalValue = url;
+    originalValue = urlPart;
   }
 
-  const { host, pathname, search } = parse(url);
+  const { host, pathname, search } = parse(urlPart);
   const queryDelimiter = !search ? '?' : '&';
 
   // The url parser returns null if the search is empty. Change that to an empty
   // string so that we can use it to build the values later
-  if (search && search.toLowerCase().indexOf(tenantKey) > -1) {
+  if (search && search.toLowerCase().indexOf('security_tenant') > -1) {
     // If we for some reason already have a tenant in the URL we skip any updates
     return originalValue;
   }
 
   // A helper for finding the part in the string that we want to extend/replace
   const valueToReplace = host! + pathname! + (search || '');
-  const replaceWith = valueToReplace + queryDelimiter + tenantKeyAndValue;
+  const replaceWith =
+    valueToReplace + queryDelimiter + 'security_tenant=' + encodeURIComponent(tenant);
 
-  return originalValue.replace(valueToReplace, replaceWith);
+  const range = document.createRange() as any;
+  const referenceNode = document.getElementsByTagName('span').item(0);
+
+  range.selectNode(referenceNode);
+  shareButton.removeAllRanges();
+  shareButton.addRange(range);
+
+  const newValue = originalValue.replace(valueToReplace, replaceWith);
+
+  if (newValue !== originalValue) {
+    target.textContent = newValue;
+  }
 }
--- public/services/test/shared-link.test.ts
+++ public/services/test/shared-link.test.ts
@@ -12,8 +12,90 @@
  *   express or implied. See the License for the specific language governing
  *   permissions and limitations under the License.
  */
+// Import necessary modules and dependencies
+import { API_ENDPOINT_MULTITENANCY } from '../../apps/configuration/constants.tsx';
+import { addTenantToShareURL, processCopyEvent } from '../shared-link.ts';
+import { updateClipboard } from '../shared-link.ts';
 
-beforeEach(() => {});
-describe('Shared link test page', () => {
-  it('tests the shared link button', () => {});
+describe('addTenantToShareURL function', () => {
+  it('should add a listener for copy events', () => {
+    const coreMock: any = {
+      http: {
+        get: jest.fn().mockResolvedValue('mocked-tenant'),
+      },
+    };
+
+    jest.spyOn(document, 'addEventListener').mockImplementation((event, callback) => {
+      if (event === 'copy') {
+        callback(new Event('copy'));
+        expect(coreMock.http.get).toHaveBeenCalledWith(API_ENDPOINT_MULTITENANCY);
+      }
+    });
+
+    addTenantToShareURL(coreMock);
+
+    // Add assertions as needed
+  });
+
+  // Add more tests for other scenarios if necessary
+});
+
+describe('processCopyEvent function', () => {
+  it('should update the clipboard and target text content', () => {
+    const shareButtonMock: any = {
+      getAttribute: jest.fn().mockReturnValue('mocked-share-url'),
+    };
+
+    const targetMock: any = {
+      textContent: 'mocked-text-content',
+    };
+
+    jest.spyOn(document, 'querySelector').mockImplementation((selector) => {
+      if (selector === '[data-share-url]') {
+        return shareButtonMock;
+      } else if (selector === 'body > span') {
+        return targetMock;
+      }
+    });
+
+    processCopyEvent('mocked-tenant');
+
+    // Add assertions based on the behavior of processCopyEvent
+    // Add more assertions as needed
+  });
+
+  // Add more tests for other scenarios if necessary
+});
+
+describe('updateClipboard function', () => {
+  it('should update the clipboard and target text content', () => {
+    const shareButtonMock: any = {
+      getAttribute: jest.fn().mockReturnValue('mocked-share-url'),
+      removeAllRanges: jest.fn(),
+      addRange: jest.fn(),
+    };
+
+    const targetMock: any = {
+      textContent: 'mocked-text-content',
+    };
+
+    jest.spyOn(document, 'querySelector').mockImplementation((selector) => {
+      if (selector === '[data-share-url]') {
+        return shareButtonMock;
+      } else if (selector === 'body > span') {
+        return targetMock;
+      }
+    });
+
+    jest.spyOn(document, 'createRange').mockReturnValue({
+      selectNode: jest.fn(),
+    } as any);
+
+    updateClipboard('mocked-url-part', 'mocked-original-value', 'mocked-tenant');
+
+    // Add assertions based on the behavior of updateClipboard
+    // Add more assertions as needed
+  });
+
+  // Add more tests for other scenarios if necessary
 });
