88b1e6523f40b22601f1ab2802a04113e76795c4
Ryan Liang <jiallian@amazon.com>
2023-11-24 10:54:55 -0800

    Add more security commands
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- .cypress/support/commands.js
+++ .cypress/support/commands.js
@@ -41,6 +41,33 @@ Cypress.Commands.overwrite('request', (originalFn, ...args) => {
   return originalFn(Object.assign({}, defaults, options));
 });
 
+Cypress.Commands.add('createTenant', (tenantID, tenantJson) => {
+  cy.request(
+    'PUT',
+    `${Cypress.env('openSearchUrl')}${SEC_API.TENANTS_BASE}/${tenantID}`,
+    tenantJson
+  );
+  cy.wait(10000);
+});
+
+Cypress.Commands.add('createInternalUser', (userID, userJson) => {
+  cy.request(
+    'PUT',
+    `${Cypress.env('openSearchUrl')}${SEC_API.INTERNALUSERS_BASE}/${userID}`,
+    userJson
+  );
+  cy.wait(10000);
+});
+
+Cypress.Commands.add('createRole', (roleID, roleJson) => {
+  cy.request(
+    'PUT',
+    `${Cypress.env('openSearchUrl')}${SEC_API.ROLE_BASE}/${roleID}`,
+    roleJson
+  );
+  cy.wait(10000);
+});
+
 Cypress.Commands.add('createRoleMapping', (roleID, rolemappingJson) => {
   cy.request(
     'PUT',
--- .cypress/support/index.d.ts
+++ .cypress/support/index.d.ts
@@ -4,13 +4,49 @@
 declare namespace Cypress {
   interface Chainable<Subject> {
     /**
+     * Create a test tenant by calling REST API
+     * @example
+     * cy.createTenant('test_tenant', tenantJsonFixture )
+     */
+     createTenant<S = any>(
+        tenantID: string,
+        tenantJson: string
+      ): Chainable<S>;
+  }
+
+  interface Chainable<Subject> {
+    /**
+     * Create an internal user by calling REST API
+     * @example
+     * cy.createInternalUser('test_user', userJsonFixture )
+     */
+     createInternalUser<S = any>(
+        userID: string,
+        userJson: string
+      ): Chainable<S>;
+  }
+
+  interface Chainable<Subject> {
+    /**
+     * Create a role by calling REST API
+     * @example
+     * cy.createRole('role_name', roleJsonFixture )
+     */
+     createRole<S = any>(
+        roleID: string,
+        roleJson: string
+      ): Chainable<S>;
+  }
+
+  interface Chainable<Subject> {
+    /**
      * Create a role mapping by calling REST API
      * @example
      * cy.createRoleMapping('role_name', rolemappingJsonFixture )
      */
-    createRoleMapping<S = any>(
-       roleID: string,
-       rolemappingJson: string
-     ): Chainable<S>;
+     createRoleMapping<S = any>(
+        roleID: string,
+        rolemappingJson: string
+      ): Chainable<S>;
   }
 }
