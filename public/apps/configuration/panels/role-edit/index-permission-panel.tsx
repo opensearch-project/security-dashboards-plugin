09b2f59453b4b3d356ba7627d1e1af663fb075bd
Yulong Ruan <ruanyu1@gmail.com>
2024-01-04 00:23:18 +0800

    fix Cannot find module when import ResourceType in server from public folder (#1705)
    * fix Cannot find module
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * fix prettier error
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * fix failed tests
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    Co-authored-by: Craig Perkins <cwperx@amazon.com>

--- public/apps/configuration/panels/role-edit/index-permission-panel.tsx
+++ public/apps/configuration/panels/role-edit/index-permission-panel.tsx
@@ -25,7 +25,8 @@ import {
 } from '@elastic/eui';
 import React, { Dispatch, Fragment, SetStateAction } from 'react';
 import { isEmpty } from 'lodash';
-import { RoleIndexPermission, ResourceType } from '../../types';
+import { RoleIndexPermission } from '../../types';
+import { ResourceType } from '../../../../../common';
 import {
   appendElementToArray,
   removeElementFromArray,
@@ -141,9 +142,9 @@ export function IndexPermissionRow(props: {
   return (
     <FormRow
       headerText="Index permissions"
-      headerSubText="You can specify permissions using both action groups or single permissions. 
+      headerSubText="You can specify permissions using both action groups or single permissions.
         A permission group is a list of single permissions.
-        You can often achieve your desired security posture using some combination of the default permission groups. 
+        You can often achieve your desired security posture using some combination of the default permission groups.
         You can also create your own reusable permission groups."
     >
       <EuiFlexGroup>
