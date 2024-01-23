f854cb3082452d263129b24f51c98a2786e281ad
Shawn Houston <56267251+shoustech@users.noreply.github.com>
2024-01-23 15:54:53 -0500

    Addressing spelling mistakes in server code. (#1753)
    * Addressing spelling mistakes in server code.
    
    Signed-off-by: Shawn Houston <shouston@neqterlabs.com>
    
    * Corrected cammel case of renamed variable.
    
    Signed-off-by: Shawn Houston <shouston@neqterlabs.com>
    
    ---------
    
    Signed-off-by: Shawn Houston <shouston@neqterlabs.com>

--- server/auth/types/openid/helper.ts
+++ server/auth/types/openid/helper.ts
@@ -114,8 +114,8 @@ export function composeLogoutUrl(
   idpEndsessionEndpoint: string | undefined,
   additionalQueryParams: any
 ) {
-  const logoutEndpont = customLogoutUrl || idpEndsessionEndpoint;
-  const logoutUrl = new URL(logoutEndpont!);
+  const logoutEndpoint = customLogoutUrl || idpEndsessionEndpoint;
+  const logoutUrl = new URL(logoutEndpoint!);
   Object.keys(additionalQueryParams).forEach((key) => {
     logoutUrl.searchParams.append(key, additionalQueryParams[key] as string);
   });
