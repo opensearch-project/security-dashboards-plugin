2db9d844e4fd9907b54848e2f2c4f9813cb011bb
David Osorno <daosgu27@gmail.com>
2024-02-05 14:54:22 -0800

    Change to elastic components based on feedback received
    Signed-off-by: David Osorno <daosgu27@gmail.com>

--- public/apps/configuration/panels/auth-view/signin-options-modal.tsx
+++ public/apps/configuration/panels/auth-view/signin-options-modal.tsx
@@ -1,32 +1,28 @@
 /*
- *   Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
  *
- *   Licensed under the Apache License, Version 2.0 (the "License").
- *   You may not use this file except in compliance with the License.
- *   A copy of the License is located at
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   or in the "license" file accompanying this file. This file is distributed
- *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *   express or implied. See the License for the specific language governing
- *   permissions and limitations under the License.
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  */
 
 import React, { Dispatch, SetStateAction } from 'react';
 import { DashboardOption } from '../../types';
-import { columns } from './dashboard-signin-options';
+
 import {
-  OuiButtonEmpty,
-  OuiButton,
-  OuiModal,
-  OuiModalBody,
-  OuiModalFooter,
-  OuiModalHeader,
-  OuiModalHeaderTitle,
-  OuiSpacer,
-  OuiCheckboxGroup
-} from '@opensearch-project/oui';
+  EuiButton,
+  EuiInMemoryTable,
+  EuiModal,
+  EuiModalBody,
+  EuiModalFooter,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiSpacer,
+} from '@elastic/eui';
+import { columns } from './dashboard-signin-options';
 
 interface DashboardSignInProps {
   dashboardOptions: DashboardOption[];
@@ -62,28 +58,29 @@ export function SignInOptionsModal(props: DashboardSignInProps): JSX.Element {
 
   if (isModalVisible) {
     modal = (
-      <OuiModal onClose={closeModal}>
-        <OuiModalHeader>
-          <OuiModalHeaderTitle>Dashboards sign-in options</OuiModalHeaderTitle>
-        </OuiModalHeader>
-        <OuiModalBody>
-          Select one or multiple authentication options to appear on the sign-in window for OpenSearch Dashboards.
-          <OuiSpacer />
-
-          <OuiCheckboxGroup
-            options={props.dashboardOptions.map((option) => ({
-              label: option.name,
-              value: option,
-              checked: option.status,
-            }))}
-            onChange={(selectedOptions) => {
-              setNewSignInOptions(selectedOptions.map((option) => option.value));
+      <EuiModal onClose={closeModal}>
+        <EuiModalHeader>
+          <EuiModalHeaderTitle>Dashboards sign-in options</EuiModalHeaderTitle>
+        </EuiModalHeader>
+        <EuiModalBody>
+          Select one or multiple authentication options to appear on the sign-in window for
+          OpenSearch Dashboards.
+          <EuiSpacer />
+          <EuiInMemoryTable
+            tableLayout={'auto'}
+            columns={columns.slice(0, 1)}
+            items={props.dashboardOptions}
+            itemId={'name'}
+            selection={{
+              onSelectionChange: setNewSignInOptions,
+              initialSelected: actualSignInOptions,
             }}
+            sorting={{ sort: { field: 'displayName', direction: 'asc' } }}
           />
-        </OuiModalBody>
-        <OuiModalFooter>
-          <OuiButtonEmpty onClick={closeModal}>Cancel</OuiButtonEmpty>
-          <OuiButton
+        </EuiModalBody>
+        <EuiModalFooter>
+          <EuiButton onClick={closeModal}>Cancel</EuiButton>
+          <EuiButton
             data-testid="update"
             onClick={() => {
               props.handleUpdate(newSignInOptions);
@@ -93,16 +90,16 @@ export function SignInOptionsModal(props: DashboardSignInProps): JSX.Element {
             disabled={disableUpdate}
           >
             Update
-          </OuiButton>
-        </OuiModalFooter>
-      </OuiModal>
+          </EuiButton>
+        </EuiModalFooter>
+      </EuiModal>
     );
   }
   return (
     <div>
-      <OuiButton data-testid="edit" onClick={showModal}>
+      <EuiButton data-testid="edit" onClick={showModal}>
         Edit
-      </OuiButton>
+      </EuiButton>
       {modal}
     </div>
   );
