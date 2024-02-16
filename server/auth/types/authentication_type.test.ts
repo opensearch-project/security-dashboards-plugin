804cce78e642f3e303a6e3b5b88ae1028860f723
Derek Ho <dxho@amazon.com>
2024-02-16 11:43:48 -0500

    Remove test after PR changed
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- server/auth/types/authentication_type.test.ts
+++ server/auth/types/authentication_type.test.ts
@@ -17,7 +17,6 @@ import { SecurityPluginConfigType } from '../..';
 import { AuthenticationType } from './authentication_type';
 import { httpServerMock } from '../../../../../src/core/server/mocks';
 import { OpenSearchDashboardsRequest } from '../../../../../src/core/server';
-import { SecuritySessionCookie } from '../../session/security_cookie';
 
 class DummyAuthType extends AuthenticationType {
   authNotRequired(request: OpenSearchDashboardsRequest): boolean {
@@ -113,51 +112,5 @@ describe('test tenant header', () => {
     };
     const result = await dummyAuthType.authHandler(request, response, toolkit);
     expect(result.requestHeaders.securitytenant).toEqual('dummy_tenant');
-  });
-
-  it(`keepalive should not shorten the cookie expiry`, async () => {
-    const realDateNow = Date.now.bind(global.Date);
-    const dateNowStub = jest.fn(() => 0);
-    global.Date.now = dateNowStub;
-
-    const keepAliveConfig = {
-      multitenancy: {
-        enabled: true,
-      },
-      auth: {
-        unauthenticated_routes: [] as string[],
-      },
-      session: {
-        keepalive: true,
-        ttl: 1000,
-      },
-    } as SecurityPluginConfigType;
-    const keepAliveDummyAuth = new DummyAuthType(
-      keepAliveConfig,
-      new BrowserSessionStorageFactory('security_cookie'),
-      router,
-      esClient,
-      coreSetup,
-      logger
-    );
-    const testCookie: SecuritySessionCookie = {
-      credentials: {
-        authHeaderValueExtra: true,
-      },
-      expiryTime: 2000,
-    };
-    // Set cookie
-    sessionStorage.setItem('security_cookie', JSON.stringify(testCookie));
-    const request = httpServerMock.createOpenSearchDashboardsRequest({
-      path: '/internal/v1',
-    });
-    const response = jest.fn();
-    const toolkit = {
-      authenticated: jest.fn((value) => value),
-    };
-    await keepAliveDummyAuth.authHandler(request, response, toolkit);
-    const cookieAfterRequest = sessionStorage.getItem('security_cookie');
-    expect(JSON.parse(cookieAfterRequest!).expiryTime).toBe(2000);
-    global.Date.now = realDateNow;
   });
 });
