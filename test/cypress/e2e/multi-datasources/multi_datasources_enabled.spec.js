31b5672a5b192aaef248ac2a743404e1d46606b6
Derek Ho <dxho@amazon.com>
2024-04-11 14:02:46 -0400

    Find by warning
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- test/cypress/e2e/multi-datasources/multi_datasources_enabled.spec.js
+++ test/cypress/e2e/multi-datasources/multi_datasources_enabled.spec.js
@@ -39,9 +39,7 @@ const createDataSource = () => {
 
 const closeToast = () => {
   // remove browser incompatibiltiy toast causing flakyness (cause it has higher z-index than Create button making it invisible)
-  cy.get('.euiToast')
-    .contains('Your browser does not meet the security requirements for OpenSearch Dashboards')
-    .find('[data-test-subj="toastCloseButton"]')
+  cy.get('[class="euiToast euiToast--warning euiGlobalToastListItem"]').find('[data-test-subj="toastCloseButton"]').first()
     .click();
 };
 
