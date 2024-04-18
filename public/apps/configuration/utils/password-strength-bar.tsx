6284fa7d72ff871b35380f0ab76d08cb17de0a14
Derek Ho <dxho@amazon.com>
2024-04-18 10:49:03 -0400

    Update the color and padding
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- public/apps/configuration/utils/password-strength-bar.tsx
+++ public/apps/configuration/utils/password-strength-bar.tsx
@@ -13,7 +13,7 @@
  *   permissions and limitations under the License.
  */
 
-import { EuiFlexGroup, EuiFlexItem, EuiProgress, EuiText } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiProgress, EuiSpacer, EuiText } from '@elastic/eui';
 import React from 'react';
 import zxcvbn from 'zxcvbn';
 
@@ -26,32 +26,39 @@ export const PasswordStrengthBar = (props: PasswordStrengthBarProps) => {
   const passwordStrength = zxcvbn(password);
   const strength = passwordStrength.score;
   let message;
+  let color;
   switch (strength) {
     case 0:
       message = 'Very weak';
+      color = 'danger';
       break;
     case 1:
       message = 'Weak';
+      color = 'danger';
       break;
     case 2:
       message = 'Ok';
+      color = 'warning';
       break;
     case 3:
       message = 'Strong';
+      color = 'success';
       break;
     case 4:
       message = 'Very strong';
+      color = 'success';
       break;
   }
 
   return (
     password && (
-      <EuiFlexGroup direction="column">
+      <EuiFlexGroup direction="column" gutterSize="xs">
         <EuiFlexItem>
           <EuiProgress
             value={strength}
             max={4}
             size="m"
+            color={color}
             valueText={message}
             label={'Password strength'}
             data-test-subj="password-strength-progress"
@@ -64,6 +71,7 @@ export const PasswordStrengthBar = (props: PasswordStrengthBarProps) => {
             </EuiText>
           </EuiFlexItem>
         )}
+        <EuiSpacer size="s" />
       </EuiFlexGroup>
     )
   );
