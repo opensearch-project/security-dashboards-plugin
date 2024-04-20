37b75b6f87311c1347a3c55ccefca86dcb9ef9cd
Craig Perkins <cwperx@amazon.com>
2024-04-20 09:35:42 -0400

    Remove basePath in acs endpoint and fix shorten URL
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- test/cypress/support/constants.js
+++ test/cypress/support/constants.js
@@ -43,6 +43,6 @@ export const SEC_API = {
   ROLE_MAPPING_BASE: `${SEC_API_PREFIX}/rolesmapping`,
 };
 export const DASHBOARDS_API = {
-  SHORTEN_URL: '/api/shorten_url',
+  SHORTEN_URL: `${basePath}/api/shorten_url`,
 };
 export const SHORTEN_URL_DATA = { url: `${basePath}/app/home#/tutorial_directory` };
