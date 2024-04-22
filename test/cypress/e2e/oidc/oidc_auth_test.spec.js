1be2ba2458d7b531cc15901430add8cc9800fae1
Craig Perkins <cwperx@amazon.com>
2024-04-21 21:54:30 -0400

    Wait for logout
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- test/cypress/e2e/oidc/oidc_auth_test.spec.js
+++ test/cypress/e2e/oidc/oidc_auth_test.spec.js
@@ -27,9 +27,9 @@ describe('Log in via OIDC', () => {
   });
 
   const kcLogin = () => {
-    cy.origin('http://127.0.0.1:8080', () => {
+    cy.origin('http://localhost:8080', () => {
       const login = 'admin';
-      const password = 'admin';
+      const password = 'Pa55w0rd';
 
       cy.get('#kc-page-title').should('be.visible');
       cy.get('input[id=username]').should('be.visible').type(login);
@@ -115,11 +115,11 @@ describe('Log in via OIDC', () => {
 
     cy.get('button[id="user-icon-btn"]').click();
 
+    cy.intercept('GET', `${basePath}/auth/openid/logout`).as('openidLogout');
+
     cy.get('button[data-test-subj^="log-out-"]').click();
 
-    cy.visit(`http://localhost:5601${basePath}/app/opensearch_dashboards_overview#/`, {
-      failOnStatusCode: false,
-    });
+    cy.wait('@openidLogout').then(() => {});
 
     kcLogin();
 
