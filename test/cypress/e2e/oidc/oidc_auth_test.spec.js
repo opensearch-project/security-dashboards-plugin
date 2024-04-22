2f88fbfacbc769fe28bc76f6dab0d4afa92eefa4
Craig Perkins <cwperx@amazon.com>
2024-04-21 22:18:55 -0400

    Small update
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- test/cypress/e2e/oidc/oidc_auth_test.spec.js
+++ test/cypress/e2e/oidc/oidc_auth_test.spec.js
@@ -27,9 +27,9 @@ describe('Log in via OIDC', () => {
   });
 
   const kcLogin = () => {
-    cy.origin('http://localhost:8080', () => {
+    cy.origin('http://127.0.0.1:8080', () => {
       const login = 'admin';
-      const password = 'Pa55w0rd';
+      const password = 'admin';
 
       cy.get('#kc-page-title').should('be.visible');
       cy.get('input[id=username]').should('be.visible').type(login);
