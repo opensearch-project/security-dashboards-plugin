opensearch-project/security-dashboards-plugin

commit a202d4875ae42729994b64fb38090af6b7f1ae7a

Remove tenant tab when disabled via yaml (#1960)
---------

Signed-off-by: Derek Ho <dxho@amazon.com>

public/apps/configuration/app-router.tsx
@@ -70,10 +70,6 @@ const ROUTE_MAP: { [key: string]: RouteItem } = {
     name: 'Tenants',
     href: buildUrl(ResourceType.tenants),
   },
-  [ResourceType.tenantsManageTab]: {
-    name: 'TenantsManageTab',
-    href: buildUrl(ResourceType.tenantsManageTab),
-  },
   [ResourceType.tenantsConfigureTab]: {
     name: '',
     href: buildUrl(ResourceType.tenantsConfigureTab),
@@ -88,22 +84,27 @@ const ROUTE_MAP: { [key: string]: RouteItem } = {
   },
 };
 
-const ROUTE_LIST = [
-  ROUTE_MAP.getStarted,
-  ROUTE_MAP[ResourceType.auth],
-  ROUTE_MAP[ResourceType.roles],
-  ROUTE_MAP[ResourceType.users],
-  ROUTE_MAP[ResourceType.serviceAccounts],
-  ROUTE_MAP[ResourceType.permissions],
-  ROUTE_MAP[ResourceType.tenants],
-  ROUTE_MAP[ResourceType.auditLogging],
-  ROUTE_MAP[ResourceType.tenantsConfigureTab],
-];
+const getRouteList = (multitenancyEnabled: boolean) => {
+  return [
+    ROUTE_MAP.getStarted,
+    ROUTE_MAP[ResourceType.auth],
+    ROUTE_MAP[ResourceType.roles],
+    ROUTE_MAP[ResourceType.users],
+    ROUTE_MAP[ResourceType.serviceAccounts],
+    ROUTE_MAP[ResourceType.permissions],
+    ...(multitenancyEnabled ? [ROUTE_MAP[ResourceType.tenants]] : []),
+    ROUTE_MAP[ResourceType.auditLogging],
+  ];
+};
 
-const allNavPanelUrls = ROUTE_LIST.map((route) => route.href).concat([
-  buildUrl(ResourceType.auditLogging) + SUB_URL_FOR_GENERAL_SETTINGS_EDIT,
-  buildUrl(ResourceType.auditLogging) + SUB_URL_FOR_COMPLIANCE_SETTINGS_EDIT,
-]);
+export const allNavPanelUrls = (multitenancyEnabled: boolean) =>
+  getRouteList(multitenancyEnabled)
+    .map((route) => route.href)
+    .concat([
+      buildUrl(ResourceType.auditLogging) + SUB_URL_FOR_GENERAL_SETTINGS_EDIT,
+      buildUrl(ResourceType.auditLogging) + SUB_URL_FOR_COMPLIANCE_SETTINGS_EDIT,
+      ...(multitenancyEnabled ? [buildUrl(ResourceType.tenantsConfigureTab)] : []),
+    ]);
 
 export function getBreadcrumbs(
   resourceType?: ResourceType,
@@ -155,21 +156,46 @@ export const LocalCluster = { label: 'Local cluster', id: '' };
 export const DataSourceContext = createContext<DataSourceContextType | null>(null);
 
 export function AppRouter(props: AppDependencies) {
+  const multitenancyEnabled = props.config.multitenancy.enabled;
   const dataSourceEnabled = !!props.depsStart.dataSource?.dataSourceEnabled;
   const setGlobalBreadcrumbs = flow(getBreadcrumbs, props.coreStart.chrome.setBreadcrumbs);
   const dataSourceFromUrl = dataSourceEnabled ? getDataSourceFromUrl() : LocalCluster;
 
   const [dataSource, setDataSource] = useState<DataSourceOption>(dataSourceFromUrl);
 
+  function getTenancyRoutes() {
+    if (multitenancyEnabled) {
+      return (
+        <>
+          <Route
+            path={ROUTE_MAP.tenants.href}
+            render={() => {
+              setGlobalBreadcrumbs(ResourceType.tenants);
+              return <TenantList tabID={'Manage'} {...props} />;
+            }}
+          />
+          <Route
+            path={ROUTE_MAP.tenantsConfigureTab.href}
+            render={() => {
+              setGlobalBreadcrumbs(ResourceType.tenants);
+              return <TenantList tabID={'Configure'} {...props} />;
+            }}
+          />
+        </>
+      );
+    }
+    return null;
+  }
+
   return (
     <DataSourceContext.Provider value={{ dataSource, setDataSource }}>
       <Router basename={props.params.appBasePath}>
         <EuiPage>
-          {allNavPanelUrls.map((route) => (
+          {allNavPanelUrls(multitenancyEnabled).map((route) => (
             // Create different routes to update the 'selected' nav item .
             <Route key={route} path={route} exact>
               <EuiPageSideBar>
-                <NavPanel items={ROUTE_LIST} />
+                <NavPanel items={getRouteList(multitenancyEnabled)} />
               </EuiPageSideBar>
             </Route>
           ))}
@@ -268,26 +294,13 @@ export function AppRouter(props: AppDependencies) {
                 }}
               />
               <Route
-                path={ROUTE_MAP.tenants.href}
-                render={() => {
-                  setGlobalBreadcrumbs(ResourceType.tenants);
-                  return <TenantList tabID={'Manage'} {...props} />;
-                }}
-              />
-              <Route
-                path={ROUTE_MAP.tenantsConfigureTab.href}
-                render={() => {
-                  setGlobalBreadcrumbs(ResourceType.tenants);
-                  return <TenantList tabID={'Configure'} {...props} />;
-                }}
-              />
-              <Route
                 path={ROUTE_MAP.getStarted.href}
                 render={() => {
                   setGlobalBreadcrumbs();
                   return <GetStarted {...props} />;
                 }}
               />
+              {getTenancyRoutes()}
               <Redirect exact from="/" to={LANDING_PAGE_URL} />
             </Switch>
           </EuiPageBody>

.github/workflows/cypress-test-tenancy-disabled.yml
@@ -83,4 +83,4 @@ jobs:
           git clone https://github.com/opensearch-project/opensearch-dashboards-functional-test.git
           cd opensearch-dashboards-functional-test
           npm install cypress --save-dev
-          yarn cypress:run-with-security --browser chrome --spec "cypress/integration/plugins/security-dashboards-plugin/inaccessible_tenancy_features.js"
+          yarn cypress:run-with-security --browser chrome --spec "cypress/integration/plugins/security/inaccessible_tenancy_features.js"

public/apps/configuration/panels/get-started.tsx
@@ -276,39 +276,41 @@ export function GetStarted(props: AppDependencies) {
 
         <EuiSpacer size="l" />
 
-        <EuiPanel paddingSize="l">
-          <EuiTitle size="s">
-            <h3>Optional: Multi-tenancy</h3>
-          </EuiTitle>
-          <EuiText size="s" color="subdued">
-            <p>
-              By default tenancy is activated in Dashboards. Tenants in OpenSearch Dashboards are
-              spaces for saving index patterns, visualizations, dashboards, and other OpenSearch
-              Dashboards objects.
-            </p>
-            <EuiFlexGroup gutterSize="s">
-              <EuiFlexItem grow={false}>
-                <EuiButton
-                  fill
-                  onClick={() => {
-                    window.location.href = buildHashUrl(ResourceType.tenants);
-                  }}
-                >
-                  Manage Multi-tenancy
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiButton
-                  onClick={() => {
-                    window.location.href = buildHashUrl(ResourceType.tenantsConfigureTab);
-                  }}
-                >
-                  Configure Multi-tenancy
-                </EuiButton>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiText>
-        </EuiPanel>
+        {props.config.multitenancy.enabled ? (
+          <EuiPanel paddingSize="l">
+            <EuiTitle size="s">
+              <h3>Optional: Multi-tenancy</h3>
+            </EuiTitle>
+            <EuiText size="s" color="subdued">
+              <p>
+                By default tenancy is activated in Dashboards. Tenants in OpenSearch Dashboards are
+                spaces for saving index patterns, visualizations, dashboards, and other OpenSearch
+                Dashboards objects.
+              </p>
+              <EuiFlexGroup gutterSize="s">
+                <EuiFlexItem grow={false}>
+                  <EuiButton
+                    fill
+                    onClick={() => {
+                      window.location.href = buildHashUrl(ResourceType.tenants);
+                    }}
+                  >
+                    Manage Multi-tenancy
+                  </EuiButton>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiButton
+                    onClick={() => {
+                      window.location.href = buildHashUrl(ResourceType.tenantsConfigureTab);
+                    }}
+                  >
+                    Configure Multi-tenancy
+                  </EuiButton>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiText>
+          </EuiPanel>
+        ) : null}
       </div>
       <EuiGlobalToastList toasts={toasts} toastLifeTimeMs={10000} dismissToast={removeToast} />
     </>

public/apps/configuration/panels/tenant-list/tenant-instruction-view.tsx
@@ -1,37 +1,0 @@
-/*
- *   Copyright OpenSearch Contributors
- *
- *   Licensed under the Apache License, Version 2.0 (the "License").
- *   You may not use this file except in compliance with the License.
- *   A copy of the License is located at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   or in the "license" file accompanying this file. This file is distributed
- *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *   express or implied. See the License for the specific language governing
- *   permissions and limitations under the License.
- */
-
-import React from 'react';
-import { EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
-
-export function TenantInstructionView() {
-  return (
-    <>
-      <EuiTitle size="l">
-        <h1>Tenants</h1>
-      </EuiTitle>
-
-      <EuiSpacer size="xxl" />
-
-      <EuiText textAlign="center">
-        <h2>You have not enabled multi tenancy</h2>
-      </EuiText>
-
-      <EuiText textAlign="center" size="xs" color="subdued" className="instruction-text">
-        Contact your administrator to enable multi tenancy.
-      </EuiText>
-    </>
-  );
-}

public/apps/configuration/panels/tenant-list/tenant-list.tsx
@@ -30,7 +30,6 @@ import { AppDependencies } from '../../../types';
 import { ExternalLink } from '../../utils/display-utils';
 import { DocLinks } from '../../constants';
 import { getDashboardsInfo } from '../../../../utils/dashboards-info-utils';
-import { TenantInstructionView } from './tenant-instruction-view';
 import { LocalCluster } from '../../app-router';
 import { SecurityPluginTopNavMenu } from '../../top-nav-menu';
 
@@ -59,8 +58,8 @@ export function TenantList(props: TenantListProps) {
     <>
       <EuiCallOut title="Tenancy is disabled" color="warning" iconType="iInCircle">
         <p>
-          Tenancy is currently disabled and users don&apos;t have access to this feature. To create,
-          edit tenants you must enabled tenanc throught he configure tenancy page.
+          Tenancy is currently disabled and users don&apos;t have access to this feature. Enable
+          tenancy through the configure tenancy page.
         </p>
         <EuiButton
           id="switchToConfigure"
@@ -128,10 +127,6 @@ export function TenantList(props: TenantListProps) {
       </EuiTab>
     ));
   };
-
-  if (!props.config.multitenancy.enabled) {
-    return <TenantInstructionView />;
-  }
 
   return (
     <>

public/apps/configuration/panels/tenant-list/test/__snapshots__/tenant-instruction-view.test.tsx.snap
@@ -1,31 +1,0 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`Tenant instruction view renders 1`] = `
-<Fragment>
-  <EuiTitle
-    size="l"
-  >
-    <h1>
-      Tenants
-    </h1>
-  </EuiTitle>
-  <EuiSpacer
-    size="xxl"
-  />
-  <EuiText
-    textAlign="center"
-  >
-    <h2>
-      You have not enabled multi tenancy
-    </h2>
-  </EuiText>
-  <EuiText
-    className="instruction-text"
-    color="subdued"
-    size="xs"
-    textAlign="center"
-  >
-    Contact your administrator to enable multi tenancy.
-  </EuiText>
-</Fragment>
-`;

public/apps/configuration/panels/tenant-list/test/tenant-instruction-view.test.tsx
@@ -1,25 +1,0 @@
-/*
- *   Copyright OpenSearch Contributors
- *
- *   Licensed under the Apache License, Version 2.0 (the "License").
- *   You may not use this file except in compliance with the License.
- *   A copy of the License is located at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   or in the "license" file accompanying this file. This file is distributed
- *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- *   express or implied. See the License for the specific language governing
- *   permissions and limitations under the License.
- */
-
-import { shallow } from 'enzyme';
-import React from 'react';
-import { TenantInstructionView } from '../tenant-instruction-view';
-
-describe('Tenant instruction view', () => {
-  it('renders', () => {
-    const component = shallow(<TenantInstructionView />);
-    expect(component).toMatchSnapshot();
-  });
-});

public/apps/configuration/panels/tenant-list/test/tenant-list.test.tsx
@@ -20,7 +20,6 @@ import { EuiInMemoryTable } from '@elastic/eui';
 import { useDeleteConfirmState } from '../../../utils/delete-confirm-modal-utils';
 import { Tenant } from '../../../types';
 import { TenantEditModal } from '../edit-modal';
-import { TenantInstructionView } from '../tenant-instruction-view';
 import { getDashboardsInfo } from '../../../../../utils/dashboards-info-utils';
 
 jest.mock('../../../utils/tenant-utils');
@@ -105,33 +104,6 @@ describe('Tenant list', () => {
     );
 
     expect(component.find(EuiInMemoryTable).prop('items').length).toBe(0);
-  });
-
-  it('renders when multitenancy is disabled in the opensearch_dashboards.yml', () => {
-    (getDashboardsInfo as jest.Mock).mockImplementation(() => {
-      return {
-        multitenancy_enabled: false,
-        private_tenant_enabled: true,
-        default_tenant: '',
-      };
-    });
-    const config1 = {
-      multitenancy: {
-        enabled: false,
-        tenants: {
-          enable_private: true,
-        },
-      },
-    };
-    const component = shallow(
-      <ManageTab
-        coreStart={mockCoreStart as any}
-        depsStart={{} as any}
-        params={{} as any}
-        config={config1 as any}
-      />
-    );
-    expect(component.find(TenantInstructionView).length).toBe(0);
   });
 
   it('fetch data error', (done) => {

commit 82d8b3677b48d9fc9a364d0ec03385a14cca7ed6

Fixes a small bug introduced in #1960
Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

public/apps/configuration/app-router.tsx
@@ -163,30 +163,6 @@ export function AppRouter(props: AppDependencies) {
 
   const [dataSource, setDataSource] = useState<DataSourceOption>(dataSourceFromUrl);
 
-  function getTenancyRoutes() {
-    if (multitenancyEnabled) {
-      return (
-        <>
-          <Route
-            path={ROUTE_MAP.tenants.href}
-            render={() => {
-              setGlobalBreadcrumbs(ResourceType.tenants);
-              return <TenantList tabID={'Manage'} {...props} />;
-            }}
-          />
-          <Route
-            path={ROUTE_MAP.tenantsConfigureTab.href}
-            render={() => {
-              setGlobalBreadcrumbs(ResourceType.tenants);
-              return <TenantList tabID={'Configure'} {...props} />;
-            }}
-          />
-        </>
-      );
-    }
-    return null;
-  }
-
   return (
     <DataSourceContext.Provider value={{ dataSource, setDataSource }}>
       <Router basename={props.params.appBasePath}>
@@ -300,7 +276,24 @@ export function AppRouter(props: AppDependencies) {
                   return <GetStarted {...props} />;
                 }}
               />
-              {getTenancyRoutes()}
+              {multitenancyEnabled && (
+                <Route
+                  path={ROUTE_MAP.tenants.href}
+                  render={() => {
+                    setGlobalBreadcrumbs(ResourceType.tenants);
+                    return <TenantList tabID={'Manage'} {...props} />;
+                  }}
+                />
+              )}
+              {multitenancyEnabled && (
+                <Route
+                  path={ROUTE_MAP.tenantsConfigureTab.href}
+                  render={() => {
+                    setGlobalBreadcrumbs(ResourceType.tenants);
+                    return <TenantList tabID={'Configure'} {...props} />;
+                  }}
+                />
+              )}
               <Redirect exact from="/" to={LANDING_PAGE_URL} />
             </Switch>
           </EuiPageBody>