9aab98a9d5d3a41212435e7f9816cc8618a88524
Derek Ho <dxho@amazon.com>
2024-07-25 15:03:41 -0400

    Do not register tenancy app if disabled in yml (#2057)
    * Do not register tenancy app if disabled in yml
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * Adds a test
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * Revert type and export
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * Use constants
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * Remove extra license field
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * Refactor with constants
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    ---------
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    (cherry picked from commit 0ed2cf2ad6c4e27a28a7edadbe12d643a251331e)

--- common/index.ts
+++ common/index.ts
@@ -15,6 +15,13 @@
 
 export const PLUGIN_ID = 'opensearchDashboardsSecurity';
 export const PLUGIN_NAME = 'security-dashboards-plugin';
+export const PLUGIN_GET_STARTED_APP_ID = `${PLUGIN_NAME}_getstarted`;
+export const PLUGIN_AUTH_APP_ID = `${PLUGIN_NAME}_auth`;
+export const PLUGIN_ROLES_APP_ID = `${PLUGIN_NAME}_roles`;
+export const PLUGIN_USERS_APP_ID = `${PLUGIN_NAME}_users`;
+export const PLUGIN_PERMISSIONS_APP_ID = `${PLUGIN_NAME}_permissions`;
+export const PLUGIN_TENANTS_APP_ID = `${PLUGIN_NAME}_tenants`;
+export const PLUGIN_AUDITLOG_APP_ID = `${PLUGIN_NAME}_auditlog`;
 
 export const APP_ID_LOGIN = 'login';
 export const APP_ID_CUSTOMERROR = 'customerror';
