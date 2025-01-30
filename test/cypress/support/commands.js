10d7a3dc8ad290c78d47ce8828bc17c6a36327c7
Craig Perkins <cwperx@amazon.com>
2025-01-29 20:34:57 -0500

    Wrap with cy.origin
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -80,15 +80,19 @@ Cypress.Commands.add('createRoleMapping', (roleID, rolemappingJson) => {
 });
 
 Cypress.Commands.add('loginWithSaml', () => {
-  cy.get('input[id=userName]').should('be.visible');
-  cy.get('button[id=btn-sign-in]').should('be.visible').click();
+  cy.origin('http://[::1]:7000', () => {
+    cy.get('input[id=userName]').should('be.visible');
+    cy.get('button[id=btn-sign-in]').should('be.visible').click();
+  });
 });
 
 Cypress.Commands.add('loginWithSamlMultiauth', () => {
   cy.get('a[aria-label="saml_login_button"]').should('be.visible');
   cy.get('a[aria-label="saml_login_button"]').should('be.visible').click();
-  cy.get('input[id=userName]').should('be.visible');
-  cy.get('button[id=btn-sign-in]').should('be.visible').click();
+  cy.origin('http://[::1]:7000', () => {
+    cy.get('input[id=userName]').should('be.visible');
+    cy.get('button[id=btn-sign-in]').should('be.visible').click();
+  });
 });
 
 if (Cypress.env('LOGIN_AS_ADMIN')) {
